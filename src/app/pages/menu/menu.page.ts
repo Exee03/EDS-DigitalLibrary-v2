9e238c8e308c5fd6d12dafe541ca852d295b6a8f
Exee03 <45579836+Exee03@users.noreply.github.com>
2020-01-16 14:54:53 +0800

    update side-menu

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,8 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
+  selectedPath = '';
 
-  constructor() { }
+  menu = [
+    {
+      title: 'Home',
+      url: '/menu/home',
+      icon: 'home',
+    },
+    {
+      title: 'Profile',
+      url: '/menu/profile',
+      icon: 'person',
+    },
+    {
+      title: 'Settings',
+      url: '/menu/settings',
+      icon: 'settings',
+    },
+  ];
+
+  constructor(
+    private router: Router
+  ) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event.url === '/menu') {
+        this.selectedPath = '/menu/home';
+      } else {
+        this.selectedPath = event.url;
+      }
+    });
+  }
 
   ngOnInit() {
   }
